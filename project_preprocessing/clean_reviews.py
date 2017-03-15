98607f40f63c7c42d6fcb4bd9d972767afc23f85
Polina Sonina <sarun4ik7@yandex.ru>
2017-03-15 16:05:36 +0300

    Add files via upload

--- project_preprocessing/clean_reviews.py
+++ project_preprocessing/clean_reviews.py
@@ -6,18 +6,20 @@ def clean_text(text):
     cleantext = re.sub('\n\d{1,2} из \d{1,2}[\n$]', '\n', cleantext, flags=re.DOTALL)
     cleantext = re.sub('(т\.к\.)|(т\. к\.)', 'т##к##', cleantext)
     cleantext = re.sub('(P\.S\.)|(P\. S\.)', 'P##S##', cleantext)
-    cleantext = re.sub('( [А-ЯЁ])\. ', '\\1## ', cleantext)
+    cleantext = re.sub('( [А-ЯЁA-Z])\. ', '\\1## ', cleantext)
     cleantext = re.sub('([\s\n]\d)\. ', '\\1## ', cleantext)
     cleantext = re.sub('[\.\\\(\)]', ' ', cleantext)
     cleantext = re.sub('[\"\*\»\«\[\]\t\,\?\!\;—…©]', '', cleantext)
     cleantext = re.sub('\:\s', ' ', cleantext)
     cleantext = re.sub('т##к##', 'т.к.', cleantext)
     cleantext = re.sub('P##S##', 'P.S.', cleantext)
-    cleantext = re.sub('( [А-ЯЁ])## ', '\\1. ', cleantext)
+    cleantext = re.sub('( [А-ЯЁA-Z])## ', '\\1. ', cleantext)
     cleantext = re.sub('([\s\n]\d{1,2})## ', '\\1. ', cleantext)
     cleantext = re.sub('\s{2}', ' ', cleantext)
     return cleantext
 
+
+abr = []
 path = 'reviews'
 if os.path.exists(path):
     #new_path = path + '_clean/'
@@ -30,6 +32,10 @@ if os.path.exists(path):
             fr = open(root + '/' + i, 'r', encoding = 'utf-8')
             text1 = fr.read()
             fr.close()
+            #abr += re.findall('[А-ЯЁа-яё\w\.]+?\.', text1)
             text2 = clean_text(text1)
             print(text2)
             print('-----------------------------------------------------------------')
+#abr_set = { word for word in abr }
+#for word in abr_set:
+    #print(word)
